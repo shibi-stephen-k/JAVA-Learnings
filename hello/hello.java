e24e0f92143f15e455d0a93eea9df22984b98f67
shibi-stephen-k <109950314+shibi-stephen-k@users.noreply.github.com>
2022-07-25 14:11:56 +0530

    Add files via upload

---
+++ hello/hello.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello");
+		System.out.print("bye");
+		//the output is hellobye for the above syntax
+		
+		System.out.println("");
+		
+		System.out.println("Hello");
+		System.out.println("bye");
+		//the output is Hello
+		//				bye
+						
+						
+						
+						
+	}
+
+}
