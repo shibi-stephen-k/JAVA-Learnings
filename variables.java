cd50b5ad033c0081c84f96d0efcfe0f0eb10ff9d
shibi-stephen-k <109950314+shibi-stephen-k@users.noreply.github.com>
2022-07-25 17:21:37 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,37 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		
+		int x;  //declaration
+		int b;
+		b=10;  // assignment
+		
+		int c=100; //initialization
+		
+		//int
+		int r=1;
+		
+		//float
+		float y=5.55f;
+		
+		//we can use double instead of float inorder to avoid putting f after the value
+		double z= 44.44;
+		
+		//boolean true or false
+		boolean f=true;
+		
+		//char 
+		char symbol = 'b';
+		
+		//string
+		String name="Hello my name is shibi";
+		
+		
+		
+		System.out.println(name);
+		
+
+	}
+
+}
