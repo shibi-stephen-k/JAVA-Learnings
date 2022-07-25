d009fe19e05ad78a216b5e6e9994ff889da4af99
shibi-stephen-k <109950314+shibi-stephen-k@users.noreply.github.com>
2022-07-25 18:53:23 +0530

    Add files via upload

---
+++ gui/gui.java
@@ -0,0 +1,14 @@
+package gui;
+import javax.swing.JOptionPane;
+
+public class gui {
+
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hello "+ name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		JOptionPane.showMessageDialog(null, "you are "+ age+" years old");
+	}
+
+}
