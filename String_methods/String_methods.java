a9fc0746d01d87fa49f6a55d6c1b8027c466fa1a
shibi-stephen-k <109950314+shibi-stephen-k@users.noreply.github.com>
2022-07-29 19:27:29 +0530

    Add files via upload

---
+++ String_methods/String_methods.java
@@ -0,0 +1,23 @@
+package String_methods;
+
+public class String_methods {
+
+	public static void main(String[] args) {
+		String name = "Shibi";
+		//if(name.equalsIgnoreCase("shibi"))
+		//	System.out.println("Matching");
+		//else
+		//	System.out.println("Not matching");
+		
+		// int result = name.length();
+		
+		//char result = name.charAt(0);
+		// int result = name.indexOf("b");
+		//boolean result = name.isEmpty();
+		//String result = name.toUpperCase();
+		//String result = name.toLowerCase();
+		//String result = name.replace('S','s');
+		System.out.println(result);
+	}
+
+}
