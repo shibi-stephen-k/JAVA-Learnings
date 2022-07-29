6e7e09aaa7371e674fa9d5cb963cd975ca601e56
shibi-stephen-k <109950314+shibi-stephen-k@users.noreply.github.com>
2022-07-29 21:03:43 +0530

    Add files via upload

---
+++ Methodd/Methodd.java
@@ -0,0 +1,22 @@
+package Methodd;
+
+public class Methodd {
+
+	public static void main(String[] args) {
+		//String name = "shibi";
+		//int age = 21;
+		//hello(name,age);
+		
+		int x=5;
+		int y=5;
+		System.out.println(add(x,y));
+	}
+	static int add(int a, int b) {
+		return a+b;
+	}
+	//static void hello(String name,int age) {
+		//System.out.println("Hello "+name);
+		//System.out.println("You are "+age);
+	//}
+
+}
