26772e1ec76339e8e327355203641224b48b1e2d
shibi-stephen-k <109950314+shibi-stephen-k@users.noreply.github.com>
2022-07-29 18:39:58 +0530

    Add files via upload

---
+++ array/array.java
@@ -0,0 +1,13 @@
+package array;
+
+public class array {
+
+	public static void main(String[] args) {
+		String[] cars = {"Bmw", "merc", "fennec"};
+		for(int i=0;i<=cars.length-1;i++)
+		{
+		System.out.println(cars[i]);
+	}
+
+}
+}
