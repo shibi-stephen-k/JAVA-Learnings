46c7ed88ab64f4474cd41890e137debf546acfcc
shibi-stephen-k <109950314+shibi-stephen-k@users.noreply.github.com>
2022-07-29 21:26:33 +0530

    Add files via upload

---
+++ fnal/fnal.java
@@ -0,0 +1,15 @@
+package fnal;
+
+public class fnal {
+
+	public static void main(String[] args) {
+		final double pi = 3.14;
+		//pi = 4;
+		//final keyword doesnt allow you to update the value if its 
+		//decleared with some variable
+		
+		System.out.println(pi);
+
+	}
+
+}
